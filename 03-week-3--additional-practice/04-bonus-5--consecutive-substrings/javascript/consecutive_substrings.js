bad2719ddb5a9b02494ef1f9025db320f216f6e3
Eric Krukowski <erickruk@amazon.com>
2023-12-04 05:00:28 -0500

    add consecutive substrings algorithm

--- 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
+++ 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
@@ -1,19 +1,50 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  substrings = new Set();
+  solution = [];
+  for (let i = 0; i < string.length; i++) {
+    currentSub = "";
+    for (let j = i; j < string.length; j++) {
+      currentSub += string[j];
+      substrings.add(currentSub);
+    }
+  }
+  return [...substrings];
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(""));
+
+  console.log("");
+
+  console.log("Expecting: [' ', ' a', 'a']");
+  console.log("=>", consecutiveSubstrings(" a"));
+
+  console.log("");
+
+  console.log("Expecting: [' ', ' a', ' a ', 'a', 'a ']");
+  console.log("=>", consecutiveSubstrings(" a "));
 }
 
 module.exports = consecutiveSubstrings;
 
 // Please add your pseudocode to this file
-// And a written explanation of your solution
+// iterate through array from 0 to end, and again from the outer loop to end, and add each character to a substring and the substring to the set.
+// reset the substring every outer iteration
+// convert the set to a list and return it
+
+// And a written explanation of your
+// performing it this way preserves the order since sets are ordered by when a value is added to it.
+// using a set prevents duplicates and ensures the first occurance displays first in the solution.
+// after viewing the solution, it appears duplicates are allowed, and I could just push everything to an array and not use the set.
