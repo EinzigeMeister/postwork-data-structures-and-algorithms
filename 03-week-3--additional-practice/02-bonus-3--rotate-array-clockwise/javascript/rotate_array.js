4a2f6fdfb58dc6202d38c73425bad6251d6a73a3
Eric Krukowski <erickruk@amazon.com>
2023-12-04 03:23:00 -0500

    add rotate array algorithm

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
@@ -1,5 +1,6 @@
 function rotateArray(arr, k) {
-  // type your code here
+  k = k % arr.length;
+  return arr.slice(-k).concat(arr.slice(0, -k));
 }
 
 if (require.main === module) {
@@ -16,9 +17,29 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3], 3));
+
+  console.log("");
+
+  console.log("Expecting: [1, 2, 3]");
+  console.log("=>", rotateArray([1, 2, 3], 0));
+
+  console.log("");
+
+  console.log("Expecting: [1]");
+  console.log("=>", rotateArray([1], 3));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", rotateArray([], 3));
 }
 
 module.exports = rotateArray;
 
 // Please add your pseudocode to this file
+// mod k to arr length
+// return arr[-k:] + arr[:-k] using javascript for the slicing & recombining
+
 // And a written explanation of your solution
+// you can only shift the array up to array.length times before it repeats, so modding k eliminates extra cycles.
+// slicing and concating @ the modded k will push the values to the beginning of the array, maintaining the same order
